d46ad193c9d7d31035a722f22150d7a2a22d1f71
MorralesonFire <u20221203124@usco.edu.co>
2024-04-17 18:55:43 -0500

    Triki Comppley

--- app/src/main/java/com/example/as8_trikitriki/MainActivity.java
+++ app/src/main/java/com/example/as8_trikitriki/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.as8_trikitriki;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,24 +41,101 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public String jugador ="x";
+    public boolean yahayganador = false;
     public void click (View v){
         //Primero obtener la referencia de que boton es el que se clickeo;
         Button btn = (Button)v;
-        //Bariable del jugador;
-        if(btn.getText().toString().equals("")){
-            btn.setText(jugador);
 
-           cambiarJugador();
+        if(yahayganador){
+            // Mostrar un mensaje o tomar alguna acción adicional si ya hay un ganador
+            Toast.makeText(this, "El juego ha terminado. Reinicia para jugar de nuevo.", Toast.LENGTH_SHORT).show();
+            return; // Salir del método sin realizar ninguna acción adicional
+        }else{
+            //Bariable del jugador;
+            if(btn.getText().toString().equals("")){
+                btn.setText(jugador);
+
+                cambiarJugador();
+                ganar(jugador);
+            }
         }
+
 
     }
 
     private void cambiarJugador() {
         if(jugador.equals("x")){
             jugador = "o";
+          //  ganar(jugador);
         }else{
             jugador = "x";
+          //  ganar(jugador);
         }
+    }
+
+    public void ganar(String resultado){
+        String casilla1 = btn1.getText().toString();
+        String casilla2 = btn2.getText().toString();
+        String casilla3 = btn3.getText().toString();
+        String casilla4 = btn4.getText().toString();
+        String casilla5 = btn5.getText().toString();
+        String casilla6 = btn6.getText().toString();
+        String casilla7 = btn7.getText().toString();
+        String casilla8 = btn8.getText().toString();
+        String casilla9 = btn9.getText().toString();
+
+        // Horizontales
+        if( casilla1.equals(resultado) && casilla2.equals(resultado) && casilla3.equals(resultado)){
+            Toast.makeText(this, "El ganador es "+ resultado + ".", Toast.LENGTH_SHORT).show();
+            yahayganador = true;
+        }
+        if( casilla4.equals(resultado) && casilla5.equals(resultado) && casilla6.equals(resultado)){
+            Toast.makeText(this, "El ganador es "+ resultado + ".", Toast.LENGTH_SHORT).show();
+            yahayganador = true;
+        }
+        if( casilla7.equals(resultado) && casilla8.equals(resultado) && casilla9.equals(resultado)){
+            Toast.makeText(this, "El ganador es "+ resultado + ".", Toast.LENGTH_SHORT).show();
+            yahayganador = true;
+        }
+        // Verticales
+        if( casilla1.equals(resultado) && casilla4.equals(resultado) && casilla7.equals(resultado)){
+            Toast.makeText(this, "El ganador es "+ resultado + ".", Toast.LENGTH_SHORT).show();
+            yahayganador = true;
+        }
+        if( casilla2.equals(resultado) && casilla5.equals(resultado) && casilla8.equals(resultado)){
+            Toast.makeText(this, "El ganador es "+ resultado + ".", Toast.LENGTH_SHORT).show();
+            yahayganador = true;
+        }
+        if( casilla3.equals(resultado) && casilla6.equals(resultado) && casilla9.equals(resultado)){
+            Toast.makeText(this, "El ganador es "+ resultado + ".", Toast.LENGTH_SHORT).show();
+            yahayganador = true;
+        }
+        // Diagonales
+        if( casilla1.equals(resultado) && casilla5.equals(resultado) && casilla9.equals(resultado)){
+            Toast.makeText(this, "El ganador es "+ resultado + ".", Toast.LENGTH_SHORT).show();
+            yahayganador = true;
+        }
+        if( casilla3.equals(resultado) && casilla5.equals(resultado) && casilla7.equals(resultado)){
+            Toast.makeText(this, "El ganador es "+ resultado + ".", Toast.LENGTH_SHORT).show();
+            yahayganador = true;
+        }
+    }
+
+    public void salir(View v){
+    finish();
+    }
+
+    public void reiniciar(View v){
+        btn1.setText("");
+        btn2.setText("");
+        btn3.setText("");
+        btn4.setText("");
+        btn5.setText("");
+        btn6.setText("");
+        btn7.setText("");
+        btn8.setText("");
+        btn9.setText("");
+        yahayganador = false;
     }
 }
 
